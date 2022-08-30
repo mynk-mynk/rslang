74d0bab1dcd725e987aa26f6e41be26379882fdc
Spin4ester <bakibayev.r@gmail.com>
2022-08-30 21:05:14 +0200

    refactor: refactor index controller

--- src/controllers/IndexController.ts
+++ src/controllers/IndexController.ts
@@ -1,12 +1,20 @@
+// eslint-disable-next-line import/no-cycle
+import App from '../models/App';
 import IndexView from '../views/pages';
 
 class IndexController {
-  static actionIndex() {
-    const data = {
-      data1: '0',
-      data2: 'index',
-    };
-    (document.querySelector('main') as HTMLElement).innerHTML = IndexView.draw(data);
+  private app: App;
+
+  constructor(app: App) {
+    this.app = app;
+  }
+
+  actionIndex() {
+    this.app.setRouterToElements('.start-menu li');
+
+    const main: HTMLElement | null =
+      document.querySelector<HTMLElement>('main');
+    if (main) main.innerHTML = IndexView.draw();
   }
 }
 
