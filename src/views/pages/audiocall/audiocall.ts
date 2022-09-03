3a2634bb5687f45357073dec937daf9fee00620f
Spin4ester <bakibayev.r@gmail.com>
2022-09-03 13:19:37 +0200

    refactor: correct links to images

--- src/views/pages/audiocall/audiocall.ts
+++ src/views/pages/audiocall/audiocall.ts
@@ -3,65 +3,7 @@ import { IObject } from '../../../common/interfaces/IObject';
 import { IWord } from '../../../common/interfaces/IWord';
 
 class AudiocallView {
-  static draw() {
-    return `<div class="main-container-audiocall">
-    <h1>Игра "Аудиовызов"</h1>
-    
-    <div class="game-container">
-      <img class="sound-icon" src="../../assets/images/audiocall/sound.png" alt="Sound">
-      <img class="word-img" src="" alt="Word image" width="100">
-
-      <div class="audiocall-word-description">
-        <img id="audio-btn" src="../../assets/svg/audio-speaker.svg" alt="Audio" class="audiocall-audio-icon" width="25">
-        <p>Word</p>
-      </div>
-
-      <div class="btns-container-audiocall">
-        <div class="answer1 correct-answer">
-          <img class="answer-img active" src="../../assets/images/right-icon.png" alt="Correct-icon" width="20">
-          <img class="answer-img" src="../../assets/images/wrong-icon.png" alt="Wrong-icon" width="20">
-          Answer 1
-        </div>
-        <div class="answer2 wrong-answer">
-          <img class="answer-img" src="../../assets/images/right-icon.png" alt="Correct-icon" width="20">
-          <img class="answer-img active" src="../../assets/images/wrong-icon.png" alt="Wrong-icon" width="20">
-          Answer 2
-        </div>
-        <div class="answer3">Answer 3</div>
-        <div class="answer4">Answer 4</div>
-        <div class="answer5">Answer 5</div>
-      </div>
-
-    </div>
-
-    <div class="result-container">
-      <h2 class="results-header">Ваш результат:</h2>
-      <div class="results">
-        <div class="results-description">
-          <p>Правильных ответов: 0</p>
-          <p>Ошибок: 0</p>
-          <p>Правильных ответов подряд: 0</p>
-        </div>
-        <p class="results-accuracy">100%</p>
-      </div>
-      <ul class="word-list">
-        <h3>Список слов:</h3>
-        <li>
-          <img class="audiocall-audio-icon" src="../../assets/svg/audio-speaker.svg" alt="Audio Icon" width="20">
-          <img src="../../assets/images/right-icon.png" alt="Correct Icon" width="20">
-          <p><span>Word</span> - перевод</p>
-        </li>
-        <li>
-          <img class="audiocall-audio-icon" src="../../assets/svg/audio-speaker.svg" alt="Audio Icon" width="20">
-          <img src="../../assets/images/wrong-icon.png" alt="Correct Icon" width="20">
-          <p><span>Word</span> - перевод</p>
-        </li>
-      </ul>
-    </div>
-
-  </div>`;
-  }
-
+  
   static renderStartBtn() {
     return `
     <button class="start-btn" disabled>Start</button>
@@ -71,7 +13,7 @@ class AudiocallView {
   static rightIcon() {
     return `<img
                     class="answer-img active"
-                    src="../../assets/images/right-icon.png"
+                    src="./assets/images/right-icon.png"
                     alt="Correct-icon"
                     width="20"
                     />`;
@@ -80,11 +22,12 @@ class AudiocallView {
   static wrongIcon() {
     return `<img
                     class="answer-img active"
-                    src="../../assets/images/wrong-icon.png"
+                    src="./assets/images/wrong-icon.png"
                     alt="Wrong-icon"
                     width="20"
                     />`;
   }
+
 
   static renderAudiocallDescription() {
     return `
@@ -94,12 +37,12 @@ class AudiocallView {
         <p>В игре "Аудиоколл" Ваша задача выбрать правильный перевод к услышанному слову из 5 предложенных вариантов.</p>
         <p class="controls-description">Вы можете отвечать на вопросы с помощью кнопок "1", "2", "3", "4", "5". Увидеть следующий вопрос можно при помощи клавиши "Enter"</p>
         <div class="control-keys">
-          <img src="../../assets/images/audiocall/key-one.png">
-          <img src="../../assets/images/audiocall/key-two.png">
-          <img src="../../assets/images/audiocall/key-three.png">
-          <img src="../../assets/images/audiocall/key-four.png">
-          <img src="../../assets/images/audiocall/key-five.png">
-          <img src="../../assets/images/audiocall/key-enter.png">
+          <img src="./assets/images/audiocall/key-one.png">
+          <img src="./assets/images/audiocall/key-two.png">
+          <img src="./assets/images/audiocall/key-three.png">
+          <img src="./assets/images/audiocall/key-four.png">
+          <img src="./assets/images/audiocall/key-five.png">
+          <img src="./assets/images/audiocall/key-enter.png">
         </div>
       </div>
       <h3>Пожалуйста, выберите раздел</h3>
@@ -113,7 +56,7 @@ class AudiocallView {
     <div class="audiocall-image-container">
           <img
             class="sound-icon"
-            src="../../assets/images/audiocall/sound.png"
+            src="./assets/images/audiocall/sound.png"
             alt="Sound"
           />
           </div>
@@ -123,7 +66,7 @@ class AudiocallView {
             <img
               id="audio-btn-${(<IWord>word).word}"
               class = "audio-btn-img"
-              src="../../assets/svg/audio-speaker.svg"
+              src="./assets/svg/audio-speaker.svg"
               alt="Audio"
               class="audio-icon"
               width="25"
@@ -169,9 +112,9 @@ class AudiocallView {
                 </div>
             </div>
             <div class="result-answers-container">
-              <h3><img src="../../assets/images/right-icon.png" alt="Correct Icon" width="20"> Правильные ответы</h3>
+              <h3><img src="./assets/images/right-icon.png" alt="Correct Icon" width="20"> Правильные ответы</h3>
               <div class="correct-results"></div>
-              <h3><img src="../../assets/images/wrong-icon.png" alt="Incorrect Icon" width="20"> Ошибки</h3>
+              <h3><img src="./assets/images/wrong-icon.png" alt="Incorrect Icon" width="20"> Ошибки</h3>
               <div class="incorrect-results"></div>
             </div>
         </div`;
@@ -181,7 +124,7 @@ class AudiocallView {
     return `
     <div class="word-list-item">
       <audio id="audio-word-${word.word}" src="http://localhost:4000/${word.audio}"></audio>
-      <img id="audio-btn-${word.word}" src="../../assets/svg/audio-speaker.svg" alt="Audio" class="audiocall-audio-icon" width="20">
+      <img id="audio-btn-${word.word}" src="./assets/svg/audio-speaker.svg" alt="Audio" class="audiocall-audio-icon" width="20">
       <span class="word-original word-audiocall">${word.word}</span> - <span class="word-audiocall">${word.wordTranslate}</span>
     </div>`;
   }
@@ -189,7 +132,7 @@ class AudiocallView {
   static renderIncorrectResults(word: IWord) {
     return `<div class="word-list-item">
       <audio id="audio-word-${word.word}" src="http://localhost:4000/${word.audio}"></audio>
-      <img id="audio-btn-${word.word}" src="../../assets/svg/audio-speaker.svg" alt="Audio" class="audiocall-audio-icon" width="20">
+      <img id="audio-btn-${word.word}" src="./assets/svg/audio-speaker.svg" alt="Audio" class="audiocall-audio-icon" width="20">
       <span class="word-original word-audiocall">${word.word}</span> - <span class="word-audiocall">${word.wordTranslate}</span>
   </div>`;
   }
