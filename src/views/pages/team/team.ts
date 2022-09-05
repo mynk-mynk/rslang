8a84a67f963d6378e85031854a1802163829103d
Romashkina <da_shootka@mail.ru>
2022-09-05 23:14:38 +0200

    feat: add information

--- src/views/pages/team/team.ts
+++ src/views/pages/team/team.ts
@@ -7,15 +7,48 @@ class TeamView {
     <h1>Наша команда</h1>
     <div class="team-container">
       <div class="card-container">
-        <img class="card-img" src="./assets/images/team/anonymous.png" alt="Team member picture 1" width="200">
+        <img class="card-img" src="./assets/images/team/dasha.png" alt="Team member picture 1" width="200">
         <div class="card-description">
           <p class="card-name">Name</p>
           <p>Frontend developer</p>
-          <p class="card-contribution">Because Firefox saves state objects to the user's disk so they can be restored after the user restarts the browser, we impose a size limit of 16 MiB on the serialized representation of a state object.</p>
-          <a class="card-link" href="">github</a>
+          <ul class="card-contribution">
+          <li>Team lead</li>
+          <li>Design</li>
+          <li>Textbook</li>
+          </ul>
+          <a class="card-link" href="https://github.com/mynk-mynk" target="_blank">github</a>
         </div>
       </div>
+
+      <div class="card-container">
+        <img class="card-img" src="./assets/images/team/rustam.jpg" alt="Team member picture 1" width="200">
+        <div class="card-description">
+          <p class="card-name">Name</p>
+          <p>Frontend developer</p>
+          <ul class="card-contribution">
+          <li>Backend</li>
+          <li>game Audiocall</li>
+          <li>game Sprint</li>
+          </ul>
+          <a class="card-link" href="https://github.com/spin4ester" target="_blank">github</a>
+        </div>
+      </div>
+
+      <div class="card-container">
+      <img class="card-img" src="./assets/images/team/ann.png" alt="Team member picture 1" width="200">
+      <div class="card-description">
+        <p class="card-name">Name</p>
+        <p>Frontend developer</p>
+        <ul class="card-contribution">
+          <li>App architecture</li>
+          <li>Routing</li>
+          <li>Statistics</li>
+          </ul>
+        <a class="card-link" href="https://github.com/ann-makovskaya" target="_blank">github</a>
+      </div>
     </div>
+    </div>
+
   </div>`;
   }
 }
